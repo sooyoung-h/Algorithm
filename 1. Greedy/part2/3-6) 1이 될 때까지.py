11adac3326c3c17988d9262f5ee5c023768cc2c1
sooyoung-h <77563814+sooyoung-h@users.noreply.github.com>
2021-01-27 15:18:20 +0900

    Create 3-6) 1이 될 때까지.py

---
+++ 1. Greedy/part2/3-6) 1이 될 때까지.py
@@ -0,0 +1,16 @@
+n,k = map(int, input().split())
+result = 0
+
+#최대한 많이 나누어야 함
+while True:
+    target = (n//k)*k #n에서 나머지를 뺀 값
+    result += (n-target) #나머지 ('-1'한 횟수)
+    n = target #새로운 N
+    if n < k:#나눌 수없음
+        break
+    result += 1
+    n //= k
+    
+#남은 수에서 '-1'하는 횟수
+result += (n-1)
+print(result)
