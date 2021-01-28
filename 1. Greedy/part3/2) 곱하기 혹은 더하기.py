1dd7de353036711569a052bbc1935d357903a517
sooyoung-h <77563814+sooyoung-h@users.noreply.github.com>
2021-01-28 20:55:16 +0900

    Update 2) 곱하기 혹은 더하기.py

--- 1. Greedy/part3/2) 곱하기 혹은 더하기.py
+++ 1. Greedy/part3/2) 곱하기 혹은 더하기.py
@@ -10,7 +10,7 @@ for i in range(num_N):
 #최대합 구하기, 다음 순서를 연산하는 방식으로
 max_sum = int_N[0]
 for i in range(num_N-1):
-    if int_N[i] == 0 or int_N[i] == 1:
+    if int_N[i] == 0 or int_N[i] == 1: # 0이거나 1일 경우에만 더하기연산
         max_sum += int_N[i+1]
     else:
         max_sum *= int_N[i+1]
