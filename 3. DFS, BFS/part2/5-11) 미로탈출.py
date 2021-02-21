5ea4462f11acfe8ce5d16ff1f7e8c0bd29ef103d
sooyoung-h <77563814+sooyoung-h@users.noreply.github.com>
2021-02-21 11:08:38 +0900

    Update 5-11) 미로탈출.py

--- 3. DFS, BFS/part2/5-11) 미로탈출.py
+++ 3. DFS, BFS/part2/5-11) 미로탈출.py
@@ -24,7 +24,7 @@ def bfs(x,y):
             if graph[nx][ny] == 0:
                 continue
             if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[nx][ny] + 1
+                graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
     return graph[n-1][m-1]
     
